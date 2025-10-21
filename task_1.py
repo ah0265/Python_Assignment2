18fd0287a2cbe89cd00a910cb2505e49f7adac40
Adithyaa Hariharan <adithyaa.hariharan@gmail.com>
2025-10-21 23:03:56 +0530

    uploading Task 1 and Task 2

---
+++ task_1.py
@@ -0,0 +1,9 @@
+"""
+Program to check if an inputted number is even or odd
+"""
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
---
+++ task_2.py
@@ -0,0 +1,11 @@
+"""
+Program to find sum of all integers from 1 to 50
+"""
+# initialise sum variable
+sum = 0
+
+# each int in range to sum
+for i in range(1,50+1):
+    sum += i
+    
+print(f"The sum of numbers from 1 to 50 is: {sum}")
